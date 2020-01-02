7404cb3361b55c3d968b19ea66a040fdbacc0abd
BugcreatorLTQ <bugcreator@qq.com>
2020-01-02 21:11:08 +0800

    add PlayerEquipmentSort

--- BoxSort/src/io/github/bugcreatorltq/BoxSort.java
+++ BoxSort/src/io/github/bugcreatorltq/BoxSort.java
@@ -5,32 +5,60 @@ import java.util.LinkedList;
 
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.inventory.InventoryCloseEvent;
+import org.bukkit.event.inventory.InventoryOpenEvent;
+import org.bukkit.event.inventory.InventoryType;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 
 public class BoxSort implements Listener {
 
-	@EventHandler
-	public void onCloseBox(InventoryCloseEvent e) {
-		Inventory inventory = e.getInventory();
-		ItemStack[] content = inventory.getContents();
+	// 排序物品堆数组
+	private LinkedList<ItemStack> sort(ItemStack[] content, int start, int end) {
 		LinkedList<ItemStack> items = new LinkedList<ItemStack>();
-		for (ItemStack item : content) {
-			if (item == null) {
-				continue;
+		for (int i = start; i < end; i++) {
+			if (content[i] != null) {
+				items.add(content[i]);
 			}
-			items.add(item);
 		}
 		items.sort(new Comparator<ItemStack>() {
+
 			@Override
 			public int compare(ItemStack arg0, ItemStack arg1) {
-				return arg0.getType().ordinal() - arg1.getType().ordinal();
+				return arg1.getType().ordinal() - arg0.getType().ordinal();
 			}
 		});
+		return items;
+	}
+
+	// 排序容器
+	private void sort(Inventory inventory) {
+		ItemStack[] content = inventory.getContents().clone();
 		inventory.clear();
-		for(ItemStack item : items) {
+		for (ItemStack item : sort(content, 0, content.length)) {
 			inventory.addItem(item);
+		}
+	}
+
+	// 排序玩家背包
+	private void sortPlayer(Inventory inventory) {
+
+		ItemStack[] content = inventory.getContents().clone();
+		inventory.clear();
+		for (ItemStack item : sort(content, 0, 9)) {
+			inventory.addItem(item);
+		}
+		int index = 9;
+		for (ItemStack item : sort(content, 9, content.length)) {
+			inventory.setItem(index++, item);
+		}
+	}
+
+	@EventHandler
+	public void onOpenBox(InventoryOpenEvent e) {
+		Inventory inventory = e.getInventory();
+		if (inventory.getType().equals(InventoryType.CHEST)) {
+			sort(inventory);
+			sortPlayer(e.getPlayer().getInventory());
 		}
 	}
 
